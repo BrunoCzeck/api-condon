30f356bd30c02cf26668b8689974cec6d97524af
BrunoCzeck <brunoczeck@hotmail.com>
2023-08-21 23:25:25 -0300

    mais um teste

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users')
+app.get('/teste', (req, res) => {
+    res.send('Welcome')
+})
 
 app.listen(3000)
 console.log('Rodando na porta 3000')
