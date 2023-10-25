e83b4e8b90e0f2979a2fef1dd34b13ee645575e1
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-24 22:31:39 -0300

    Add Newn

--- database/connection.cjs
+++ database/connection.cjs
@@ -1,11 +1,23 @@
 const mysql = require('mysql2')
 
+/* const pool = mysql.createPool({
+    host: process.env.DATABASE_URL,
+ user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+
+}); */
+
 const pool = mysql.createPool({
     host: process.env.DATABASE_URL,
-   /*  user: process.env.PGUSER,
+    user: process.env.PGUSER,
     database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD, */
-
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = pool.promise()
+module.exports = pool.promise();
+
