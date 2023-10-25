100ce30ad7f2698f315453bda605bd119e11ee2b
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-25 14:04:54 -0300

    new process

--- database/connection.cjs
+++ database/connection.cjs
@@ -4,9 +4,9 @@ const mysql = require('mysql')
  */
 const pool = mysql.createPool({
     host: process.env.DATABASE_URL,
- user: process.env.PGUSER,
+/*  user: process.env.PGUSER,
     database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
+    password: process.env.PGPASSWORD, */
 
 });
 
