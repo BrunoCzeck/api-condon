da168355888671f22048d7f01171a58c8fda9fdb
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-25 14:44:14 -0300

    new process

--- database/connection.cjs
+++ database/connection.cjs
@@ -1,8 +1,27 @@
-const { Pool } = require('pg')
+/* const mysql = require('mysql')
 
-dotenv.config();
+const pool = mysql.createPool({
+    host: process.env.DATABASE_URL,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
 
-const databaseConfig = { connectionString: process.env.DATABASE_URL };
-const pool = new Pool(databaseConfig);
+});
 
-export default pool;
+module.exports = pool; */
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres://postgres:b-CadfCE*d33gGf3Cc13a322Eb3ba-2b@roundhouse.proxy.rlwy.net:19596/railway'
+ /*  host: 'localhost',
+  database: 'sua_database',
+  password: 'sua_senha',
+  port: 5432, */
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
