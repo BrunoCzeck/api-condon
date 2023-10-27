d5e8fcedb55e43d02b10b9b45430a6a600292756
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-27 11:31:45 -0300

    new

--- routes/routes.condon.cjs
+++ routes/routes.condon.cjs
@@ -1,6 +1,20 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controller/UsersController.cjs")
+const cors = require("cors")
+
+
+const app = express();
+const allowedOrigins = ['http://localhost:3000', 'https://api-condon-production.up.railway.app'];
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Adicione os métodos HTTP que deseja permitir
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type,Authorization', // 
+  }));
+
 
 router.get("/", userController.getUsers)
 router.get("/:id", userController.getUserId)
