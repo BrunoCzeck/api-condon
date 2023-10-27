7eaf15205b013d49cd1dd3d789769420649a5391
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-27 10:55:21 -0300

    new

--- index.cjs
+++ index.cjs
@@ -3,8 +3,16 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 require('dotenv').config() /* Config o env */
+
+
+const allowedOrigins = ['http://localhost:3000', 'https://api-condon-production.up.railway.app'];
+
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Adicione os métodos HTTP que deseja permitir
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type,Authorization', // 
   }));
   
 const usersRouter = require('./routes/routes.condon.cjs')           /* Chama a rota usuário */
