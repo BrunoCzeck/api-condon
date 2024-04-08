812821b817593c2ef6b3f4b28a2b8c9fc34da1df
Bruno <brunoczeck@hotmail.com>
2024-04-08 18:36:04 -0300

    Ajuste dump, collection

--- index.cjs
+++ index.cjs
@@ -7,11 +7,12 @@ const bodyParser = require('body-parser');
 
 const usersRouter = require('./routes/routes.condon.cjs')           /* Chama a rota usuário */
 const muralRouter = require('./routes/routes.mural.cjs')           /* Chama a rota mural */
-const chatRouter = require('./routes/routes.chat.cjs')            /* Chama a rota chat */
 const schedulerRouter = require('./routes/routes.scheduler.cjs') /* Chama a rota scheduler */
 const enterpriseController = require('./routes/routes.enterprise.cjs') /* Chama a rota scheduler */
 const correspondenceRouter = require('./routes/routes.correspondence.cjs') /* Chama a rota scheduler */
 const meetingControllerRouter = require('./routes/routes.meeting.cjs') /* Chama a rota scheduler */
+const chatRouter = require('./routes/routes.chat.cjs')            /* Chama a rota chat */
+const votingRouter = require('./routes/routes.voting.cjs')            /* Chama a rota chat */
 
 /* app.use(express.urlencoded({extended: false}))
 app.use(express.json()) */
@@ -25,12 +26,12 @@ app.use(cors({
 
 app.use("/api/v1/users", usersRouter)
 app.use("/api/v1/mural", muralRouter)
-app.use("/api/v1/chat", chatRouter)
 app.use("/api/v1/scheduler", schedulerRouter)
 app.use("/api/v1/enterprise", enterpriseController)
 app.use("/api/v1/correspondence", correspondenceRouter)
 app.use("/api/v1/meeting", meetingControllerRouter)
-
+app.use("/api/v1/chat", chatRouter)
+app.use("/api/v1/voting", votingRouter)
 
 const PORT = process.env.PORT || 3333
 
